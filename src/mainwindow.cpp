5b34fa583519426bda7bf25dba61663236d91b48
Freddie Witherden <freddie@witherden.org>
2009-01-26 14:44:14 +0000

    Add GNU GPL v3+ licensing cruft to the source code and CC BY-SA 2.5 for icons taken from the Tango project.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,22 @@
+/*
+    This file is part of Double Pendulum.
+    Copyright (C) 2009  Freddie Witherden
+
+    Double Pendulum is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Double Pendulum is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Double Pendulum; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
