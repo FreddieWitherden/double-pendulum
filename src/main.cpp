ee5766d8e4641a68620e9dfca92dbdd8db7697ff
Freddie Witherden <freddie@witherden.org>
2009-02-01 23:13:31 +0000

    Remove some commented debugging code from the main() function.

--- src/main.cpp
+++ src/main.cpp
@@ -17,22 +17,16 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-#include <iostream>
-
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 #include "doublependulumrk4.h"
 
 int main(int argc, char *argv[])
 {
-    //DoublePendulumRK4 e(0.1, 0, 9.8, 1.0, 0, 0, 9.8, 0.00001);
-
-    //std::cout << "t = " << e.time() << " theta = " << e.theta1() << std::endl;
-    //e.update(3.14);
-    //std::cout << "t = " << e.time() << " theta = " << e.theta1() << std::endl;
-
     QApplication app(argc, argv);
+
     MainWindow w;
     w.show();
+
     return app.exec();
 }
